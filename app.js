8f24ddf363a7672c1edb650b31c0b503b0a72465
Charles Lehner <celehner1@gmail.com>
2011-12-05 21:39:12 -0500

    Add color wheel / picker from Harmony.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var canvas = document.getElementById("canvas");
+function init() {
+	var canvas = document.getElementById("canvas");
 
-var s = (Math.min(innerWidth, innerHeight) - 1) / 2;
-var x = (innerWidth - s)/2;
-var y = (innerHeight - s)/2;
+	var s = (Math.min(innerWidth, innerHeight) - 1) / 2;
+	var x = (innerWidth - s)/2;
+	var y = (innerHeight - s)/2;
 
-var initialRect = new Rect(x, y, s, s)
-var fractal = new SquareRectangleFractal("a_");
-var base = new FixedFractal(fractal, initialRect);
-var fractalView = new FractalView(canvas, base);
+	var initialRect = new Rect(x, y, s, s);
+	var fractal = new SquareRectangleFractal("a_");
+	var base = new FixedFractal(fractal, initialRect);
+	var fractalView = new FractalView(canvas, base);
 
-function updateViewport() {
-	var viewport = new Rect(0, 0, window.innerWidth, window.innerHeight);
-	fractalView.setViewport(viewport);
+	function updateViewport() {
+		var viewport = new Rect(0, 0, window.innerWidth, window.innerHeight);
+		fractalView.setViewport(viewport);
+	}
+	window.addEventListener("resize", updateViewport, false);
+	updateViewport();
+
+	var mouse = new MouseController(canvas);
+	mouse.setBehavior(new DefaultBehavior(fractalView, mouse));
 }
-window.addEventListener("resize", updateViewport, false);
-updateViewport();
-
-var mouse = new MouseController(canvas);
-mouse.setBehavior(new DefaultBehavior(fractalView, mouse));
-
